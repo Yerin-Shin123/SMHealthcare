8bbc512f7984c6eec33e76248b5f17c672ad9851
Yerin-Shin123 <yerin24680@naver.com>
2024-12-20 02:19:01 +0900

    n.7

--- base_code/cal_healthdata.c
+++ base_code/cal_healthdata.c
@@ -34,16 +34,31 @@ void saveData(const char* HEALTHFILEPATH, const HealthData* health_data) {
 
     // ToCode: to save the chosen exercise and total calories burned 
     fprintf(file, "[Exercises] \n");
-    
+    for (i = 0; i < health_data->exercise_count; i++) 
+    {
+        fprintf(file, "%s: %d Calories burned\n", health_data->exercises[i].name, health_data->exercises[i].calories_burned);
+        // by yerin, record the name of the exercise and the calories burned on file in the form of "Exercise Name: Burn Calories"
+    } 
+
     
     // ToCode: to save the chosen diet and total calories intake 
     fprintf(file, "\n[Diets] \n");
-
+    for (int i = 0; i < health_data->diet_count; i++) 
+    {
+        fprintf(file, "%s: %d calories consumed\n", health_data->diets[i].name, health_data->diets[i].calories);
+        // by yerin, record the name of meal and the calories consumed on file in the form of "Diet Name: Eating Calories."
+    } 
 
 
     // ToCode: to save the total remaining calrories
     fprintf(file, "\n[Total] \n");
-    
+    fprintf(file, "Basal Metabolic Rate (BMR): %d calories\n", health_data->basal_metabolic_rate); // by yerin, record Basal Metabolic Rate
+    fprintf(file, "Total Calories Burned: %d calories\n", health_data->total_burned_calories); // by yerin, record Total Calories Burned
+    fprintf(file, "Total Calories Consumed: %d calories\n", health_data->total_consumed_calories); // by yerin, record Total Calories consumed
+    fprintf(file, "Remaining Calories: %d calories\n", health_data->remaining_calories); //by yerin, record Remaining Calories
+
+    fclose(file);
+    // by yerin, close file
     
 }
 
